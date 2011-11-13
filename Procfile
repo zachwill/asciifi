0fe211f1a6f69bf2a32a1116a6298e810c409cbf
Zach Williams <hey@zachwill.com>
2011-11-12 22:59:08 -0800

    First commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python bootstrap.py $PORT --gevent
---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""Tests for the Flask Heroku template."""
+
+import unittest
+from app import create_app
+
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        app = create_app()
+        self.app = app.test_client()
+
+    def test_home_page_works(self):
+        rv = self.app.get('/')
+        self.assertTrue(rv.data)
+        self.assertEquals(rv.status_code, 200)
+
+    def test_404_page(self):
+        rv = self.app.get('/i-am-not-found/')
+        self.assertEquals(rv.status_code, 404)
+
+    def test_static_text_file_request(self):
+        rv = self.app.get('/humans.txt')
+        self.assertTrue(rv.data)
+        self.assertEquals(rv.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
